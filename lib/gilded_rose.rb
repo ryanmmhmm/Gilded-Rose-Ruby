24e118bf9ab6f115903886d64545eb6f6c81fd2f
Ryan Magowan <ryanm@nulogy.com>
2016-03-31 11:50:56 -0400

    aged brie behaviour successfully refactored into brie_tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,6 +15,14 @@ class GildedRose
     @quality -= 1 if @days_remaining <= 0
   end
 
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
+
   def tick
 
     case @name
