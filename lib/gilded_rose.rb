a402e374624ed3b081dcca37dacbbb5149b88069
Ryan Magowan <ryanm@nulogy.com>
2016-03-31 11:53:56 -0400

    refactoring brie_tick to better describe what's happening

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,7 +17,7 @@ class GildedRose
 
   def brie_tick
     @days_remaining -= 1
-    return if @quality >= 50
+    return if @quality == 50
 
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
