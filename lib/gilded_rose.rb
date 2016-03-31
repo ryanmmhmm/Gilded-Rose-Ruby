e322806217e54f8f5f88808018cf3ad0cd5e8a40
Ryan Magowan <ryanm@nulogy.com>
2016-03-31 11:37:19 -0400

    create seam in tick method for normal items, send message to self on normal_tick method, quickly get tests to green again

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,7 +7,25 @@ class GildedRose
     @quality = quality
   end
 
+  def normal_tick
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
+    end
+
+    @days_remaining -= 1
+  end
+
   def tick
+
+    if @name.include?("Normal")
+      return normal_tick
+    end
+
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
@@ -31,9 +49,11 @@ class GildedRose
         end
       end
     end
+
     if @name != "Sulfuras, Hand of Ragnaros"
       @days_remaining = @days_remaining - 1
     end
+
     if @days_remaining < 0
       if @name != "Aged Brie"
         if @name != "Backstage passes to a TAFKAL80ETC concert"
